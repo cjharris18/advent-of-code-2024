edb758f808d123b17a89ec05736b890a5e4572a7
Chris Harris <33553721+cjharris18@users.noreply.github.com>
2024-12-03 12:08:52 +0000

    Add Part Two for Day 1

--- solutions/day-01/solution.py
+++ solutions/day-01/solution.py
@@ -12,25 +12,47 @@ def create_lists_from_file(file_name):
     # Convert the tuples back to lists as zip returns tuples.
     return list(list_1), list(list_2)
 
-def part_one(list_1, list_2):
-    return sorted(list_1), sorted(list_2)
 
-def part_two(left_list, right_list):
+def part_one(left_list, right_list):
     # 1. the zip part creates pairs from the two lists.
     # 2. the abs part calculates the absolute difference for each pair.
-    # Absolute values are used to measure the magnitude of the difference between two values, not if one is larger or smaller.
+    # Absolute values are used to measure the magnitude of the difference between two values, not if one is larger or
+    # smaller.
     # 3. the sum adds up all these differences to get the total distance.
     total_distance = sum(abs(left - right) for left, right in zip(left_list, right_list))
 
-    return total_distance
+    print("Part One Answer: {}".format(total_distance))
+
+
+def part_two(left_list, right_list):
+    # Define the similarity score.
+    similarity_score = 0
+    # Cache to store previously computed scores
+    cache = {}
+
+    # Let's cycle through both for loops.
+    for num_left in left_list:
+        if num_left in cache:
+            similarity_score += cache[num_left]
+        else:
+            count = 0
+            for num_right in right_list:
+                if num_right > num_left:
+                    break
+                if num_left == num_right:
+                    count += 1
+
+            score = num_left * count
+            cache[num_left] = score
+            similarity_score += score
+
+    print("Part Two Answer: {}".format(similarity_score))
 
 # Create the lists from the provided input.
 list_1, list_2 = create_lists_from_file('input.txt')
 
-# Sort the lists into ascending order.
-list_1_asc, list_2_asc = part_one(list_1, list_2)
+# Solve part one and sort the lists.
+part_one(sorted(list_1), sorted(list_2))
 
-# Return the answer for the solution by using the part two function.
-answer = part_two(list_1_asc, list_2_asc)
-
-print("Day 1 Answer: {}".format(answer))
+# Solve part two and sort the lists.
+part_two(sorted(list_1), sorted(list_2))
